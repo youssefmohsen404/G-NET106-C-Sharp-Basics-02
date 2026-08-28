7ac2b70cd14b558dc0b429c33a342cc813f3d01f
youss <youssmohss111@gmail.com>
2026-08-28 22:14:47 +0300

    Question1

---
+++ G-NET106-C#Basics-02/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace G_NET106_C_Basics_02
+{
+    internal class Book
+    {
+        public string title;
+        public int pages;
+    }
+}
--- G-NET106-C#Basics-02/Program.cs
+++ G-NET106-C#Basics-02/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Question1
+            //Create a Book class with a Title (string) and Pages (int). Create a Book object and store it in a variable of type object. Print it.
+
+
+            /*Book book = new Book();
+            object obj = book;
+            Console.WriteLine(obj);*/
+            #endregion
+
         }
     }
 }
