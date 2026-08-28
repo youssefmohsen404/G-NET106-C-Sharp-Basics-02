2977bc483fff84df5f0deced8b0eb4067ffc1290
youss <youssmohss111@gmail.com>
2026-08-28 22:19:32 +0300

    Question2

--- G-NET106-C#Basics-02/Program.cs
+++ G-NET106-C#Basics-02/Program.cs
@@ -12,7 +12,16 @@
             object obj = book;
             Console.WriteLine(obj);*/
             #endregion
+            #region Question2
+            //Using the Book class above, print the result of calling ToString(), Equals() (compare book with itself), GetHashCode(), and GetType() on book.
 
+            /*Book book = new Book();
+            Console.WriteLine(book.ToString());
+            Console.WriteLine(book.Equals(book));
+            Console.WriteLine(book.GetHashCode());
+            Console.WriteLine(book.GetType());*/
+
+            #endregion
         }
     }
 }
